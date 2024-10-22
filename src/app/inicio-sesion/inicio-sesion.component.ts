dc80069fdb89a93fe88ac9ad74aaa50efec71d8b
GabyMerino <gaby.swift.2000@gmail.com>
2024-10-22 12:08:46 -0600

    pruebas

---
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-sesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicio-sesion.component.html',   
+  styleUrl: './inicio-sesion.component.css'
+})
+
+export default class InicioSesionComponent {
+
+}
